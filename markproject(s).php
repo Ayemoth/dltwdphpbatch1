e2b3f26916e6736a75cce65fea80d49516ff84b8
Ayemoth <72218478+Ayemoth@users.noreply.github.com>
2020-10-11 15:46:28 +0630

    Add files via upload

---
+++ markproject(s).php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title>Reveal Mark</title>
+</head>
+<body>
+<form action="mark(self).php" method="POST">
+<label for="mark">Mark</label>
+<input type="text" name="mark" id="mark" class="">
+<br/><br/>
+<button name="submit" id="mark" class="">Submit</button>
+</form>
+
+</body>
+<footer>
+
+
+</footer>
+</html>
+
+
+
+
+
